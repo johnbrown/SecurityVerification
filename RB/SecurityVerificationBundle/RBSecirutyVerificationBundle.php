889641a5496c970f1409ba672c07d3a56276e453
brown298 <brown.john@gmail.com>
2013-07-09 00:20:26 +0000

    defining basic bundle

---
+++ RB/SecurityVerificationBundle/RBSecirutyVerificationBundle.php
@@ -0,0 +1,13 @@
+<?php
+namespace RB\SecruityVerificationBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class RBSecirutyVerificationBundle
+ * @package RB\SecruityVerificationBundle
+ * @author  John Brown <brown.john@gmail.com>
+ */
+class RBSecirutyVerificationBundle extends Bundle
+{
+}
--- README.md
+++ README.md
@@ -2,3 +2,9 @@ SecurityVerification
 ====================
 
 Displays security information for verification purposes.
+
+INSTALL
+=======
+"require-dev": {
+    "rb/security-verification": "dev-master"
+},
