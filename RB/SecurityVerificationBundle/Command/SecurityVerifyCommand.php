01b9686e7ba63b8e96482eeb9f2933d381baa273
brown298 <brown.john@gmail.com>
2013-07-09 00:54:55 +0000

    adding base

---
+++ RB/SecurityVerificationBundle/Command/SecurityVerifyCommand.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace RB\SecruityVerificationBundle\Command;
+
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Output\Output;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+
+/**
+ * Class SecurityVerifyCommand
+ * @package RB\SecruityVerificationBundle\Command
+ * @author  John Brown <brown.john@gmail.com>
+ */
+class SecurityVerifyCommand extends ContainerAwareCommand
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this->setName('rb:security:print')
+            ->setDescription('Prints out the security associated with routes')
+            ->setHelp("");
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+
+    }
+}
